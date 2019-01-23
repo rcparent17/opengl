fcc704d3d8290ed733b1a343512c042dd05b6868
Reilly Parent <reilly.c.parent@gmail.com>
2019-01-22 21:53:32 -0500

    working source/binary Lab 1

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -1,31 +1,53 @@
 #include <GL/glut.h>
 
-int gsize = 0;
+int checkerSize = 0;
+int diamondX = 0;
+int diamondY = 0;
+int diamondR = 0;
 
 void checkerboard(void){
+  int offset = 0;
   glClear(GL_COLOR_BUFFER_BIT);
-  bool white = 1;
+
   for(int r=0; r<8; r++){
     for(int c=0; c<8; c++){
-      if(white){
-        glColor3f(1.0,1.0,1.0);
+      if(r%2){
+        offset = checkerSize;
       }
       else{
-        glColor3f(0.0,0.0,0.0);
+        offset = 0;
       }
-      glBegin(GL_POLYGON);
-      glVertex3i(r*gsize, c*gsize, 0);
-      glVertex3i(r*gsize+gsize, c*gsize, 0);
-      glVertex3i(r*gsize+gsize, c*gsize+gsize, 0);
-      glVertex3i(r*gsize, c*gsize+gsize, 0);
-      glEnd();
+      glColor3f(1.0,1.0,1.0);
+      glRecti(r*checkerSize,offset+2*c*checkerSize,r*checkerSize+checkerSize, offset+2*c*checkerSize+checkerSize);
       glFlush();
-      white = !white;
     }
   }
 }
 
-void init(void)
+void diamond(void){
+  for(int i = 0; i<100; i++){
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_POLYGON);
+    glVertex2i(diamondX-diamondR, diamondY);
+    glVertex2i(diamondX, diamondY+diamondR);
+    glVertex2i(diamondX+diamondR, diamondY);
+    glVertex2i(diamondX, diamondY-diamondR);
+    glEnd();
+    glFlush();
+    diamondX = (rand() % (8*checkerSize + 1));
+    diamondY = (rand() % (8*checkerSize + 1));
+    diamondR = (rand() % (40 - 20 + 1)) + 20;
+  }
+}
+
+void diamond(int x, int y, int size){
+  diamondX = x;
+  diamondY = y;
+  diamondR = size;
+  glutDisplayFunc(diamond);
+}
+
+void init(int size)
 {
 	/* select clearing color 	*/
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -33,19 +55,25 @@ void init(void)
 	/* initialize viewing values  */
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	glOrtho(0.0, 8*size, 0.0, 8*size, 0.0, 1.0);
+  //glTranslatef(-4.0*checkerSize,-4.0*checkerSize, 0.0);
 }
 
+void checkerboard(int size){
+  checkerSize = size;
+  glutDisplayFunc(checkerboard);
+}
 
 int main(int argc, char** argv){
-  gsize = 100;
+  int size = 100;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(8*gsize, 8*gsize);
+	glutInitWindowSize(8*size, 8*size);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Checkerboard");
-	init();
-	glutDisplayFunc(checkerboard);
+	init(size);
+	checkerboard(size);
+  //diamond(4*size, 4*size, 50);
 	glutMainLoop();
 	return 0;   /* ANSI C requires main to return int. */
 }
